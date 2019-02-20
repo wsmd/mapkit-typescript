610c562a542cdc62e750b2b2490f57b84923e502
Waseem Dahman <dwaseem@icloud.com>
2019-02-19 22:19:34 -0500

    update types root

---
+++ mapkit/index.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for MapKit JS 5.13.0
+// Project: https://developer.apple.com/documentation/mapkitjs
+// Definitions by: Waseem Dahman <https://github.com/wsmd>
+
+/// <reference path="mapkit.d.ts" />
+/// <reference path="mapkit.Map.d.ts" />
+/// <reference path="mapkit.MapRect.d.ts" />
+/// <reference path="mapkit.CoordinateRegion.d.ts" />
+/// <reference path="mapkit.Coordinate.d.ts" />
+/// <reference path="mapkit.Padding.d.ts" />
+/// <reference path="mapkit.Annotation.d.ts" />
+/// <reference path="mapkit.Overlay.d.ts" />
+/// <reference path="mapkit.CoordinateSpan.d.ts" />
+/// <reference path="mapkit.TileOverlay.d.ts" />
+/// <reference path="mapkit.Style.d.ts" />
+/// <reference path="mapkit.Geocoder.d.ts" />
+/// <reference path="mapkit.Search.d.ts" />
+/// <reference path="mapkit.Directions.d.ts" />
+/// <reference path="mapkit.MapPoint.d.ts" />
+/// <reference path="mapkit.MapRect.d.ts" />
+/// <reference path="mapkit.MapSize.d.ts" />
+/// <reference path="mapkit.BoundingRegion.d.ts" />
+/// <reference path="mapkit.GeoJSON.d.ts" />
